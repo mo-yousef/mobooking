93bc0d6b24dc37f8e494b0b1abc70d35911bdbd4
motestmo <mohammad.yousif@geomatrix.group>
2025-05-16 09:32:38 +0300

    Add title and description to MoBooking container

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<div class="mobooking-container">
+<div class="mobooking-container" data-test="mobooking">
+    <h1 class="mobooking-title">MoBooking</h1>
+    <p class="mobooking-description">MoBooking is a booking system for mobile devices.</p>
     <?php 
     if (have_posts()) :
         while (have_posts()) : the_post();
